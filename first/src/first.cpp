7c6a0c622fd87f1ad01c8ac83b0f10551d16716d
apple <apple@appledeMacBook-Pro.local>
2019-03-04 09:23:32 +0800

    simple framework developed

---
+++ first/src/first.cpp
@@ -0,0 +1,38 @@
+//============================================================================
+// Name        : first.cpp
+// Author      : Xiang Li
+// Email       : 1374327576@qq.com
+// Version     : v1.0
+// Time        : 2019-03-04
+// Description : first attempt in C++
+//============================================================================
+
+#include<cstdio>
+#define MAXN 50005
+#define MAXM 5000005
+#define INPUT_FILE_NAME "WikiData.txt"
+
+using namespace std;
+
+void input_from_file(){
+
+}
+
+void random_calculate(){
+
+}
+
+void output_to_file(){
+
+}
+
+int point[MAXN],ending[MAXM],next[MAXM];
+int ans[MAXN];
+int n,m;//the number of nodes and sides
+
+int main() {
+	input_from_file();
+	random_calculate();//answer is saved in the array ans
+	output_to_file();
+	return 0;
+}
